8e14adced96d5e92c270070f6753c344a032f80c
KadesChaos <kadeschaos@gmail.com>
2026-08-04 18:31:16 -0400

    Drop attack_cat before encoding to prevent label leakage

--- data_handler.py
+++ data_handler.py
@@ -23,12 +23,13 @@ class Preprocessor:
         self.feature_columns = None
         self.scaler_fitted = False
 
-    def preprocess_data(self, data, fit_scaler=False, is_train=False, label_column='label'):
+    def preprocess_data(self, data, fit_scaler=False, is_train=False, label_column='label', drop_columns=('attack_cat',)):
         if data is None:
             logging.warning("No data to preprocess.")
             return None, None
 
         data = data.copy()
+        data.drop(columns=[c for c in drop_columns if c in data.columns], inplace=True)  # e.g. attack_cat directly determines label -> would leak
         data.ffill(inplace=True)
         data.bfill(inplace=True)  # ffill alone leaves leading NaNs unfilled
         data_encoded = pd.get_dummies(data)
@@ -62,8 +63,8 @@ class Preprocessor:
 # (train.py, ids_system.py) that call preprocess_data(...) directly keep working.
 _default_preprocessor = Preprocessor()
 
-def preprocess_data(data, fit_scaler=False, is_train=False, label_column='label'):
-    return _default_preprocessor.preprocess_data(data, fit_scaler=fit_scaler, is_train=is_train, label_column=label_column)
+def preprocess_data(data, fit_scaler=False, is_train=False, label_column='label', drop_columns=('attack_cat',)):
+    return _default_preprocessor.preprocess_data(data, fit_scaler=fit_scaler, is_train=is_train, label_column=label_column, drop_columns=drop_columns)
 
 def data_split(features, labels, test_size=0.2, random_state=42):
     X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=test_size, random_state=random_state)
